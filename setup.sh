ae83908316509e0c101c85d8983ac21eb72ca4de
garfield.yang <ymy1019@gmail.com>
2013-11-25 00:20:40 +0800

    fix .sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
+# backup
+old='/_old_dot_file/'
+[ -d $HOME$old ]||$(mkdir $HOME$old)
+if [ -d $HOME/.vim ];then
+    mv $HOME/.vim $HOME$old/.vim
+    echo "mv .vim"
+fi
+if [ -f $HOME/.vimrc ];then
+    mv $HOME/.vimrc $HOME$old/.vimrc
+    echo "mv .vimrc"
+fi
+if [ -f $HOME/.bashrc ];then
+    mv $HOME/.bashrc $HOME$old/.bashrc
+    echo "mv .bashrc"
+fi
+if [ -f $HOME/.bash_profile ];then
+    mv $HOME/.bash_profile $HOME$old/.bash_profile
+    echo "mv .bash_profile"
+fi
+
+# link
 DIR=$(pwd)
-ln -Fs $DIR/.bahsrc ~/.bashrc
+ln -Fs $DIR/.bashrc ~/.bashrc
 ln -Fs $DIR/.bash_profile ~/.bash_profile
 ln -Fs $DIR/.vim ~/.vim
 ln -Fs $DIR/.vimrc ~/.vimrc
