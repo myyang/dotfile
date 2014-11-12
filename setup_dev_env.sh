805716c621b8f2698fe11c3281be2cd841563d33
myyang <ymy1019@gmail.com>
2014-11-12 17:49:28 +0800

    fix command and execute permission

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -6,12 +6,17 @@ echo "=====> setup  homebrew..."
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 echo "=====> setup git..."
-brew install git
-brew install git-flow
+/usr/local/bin/brew install git
+/usr/local/bin/brew install git-flow
 
 echo "=====> setup pip..."
 sudo easy_install pip
+sudo pip install yolk # yolk -V [pkg]: list availabe pkg version
 
 echo "=====> setup virtualenv & wrapper..."
-pip install virtualenv
-pip install virtualenvwrapper
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
+
+
+echo "=====> setup ruby-env, build"
+/usr/local/bin/brew install rbenv ruby-build rbenv-gem-rehash
