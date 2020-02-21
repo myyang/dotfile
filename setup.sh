35958c40d75257addfa1d9fce24bda7a40c90d41
Meng-Ying Yang <ymy1019@gmail.com>
2020-02-21 16:41:37 +0800

    setup.sh: fix shellcheck

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo -e "Start linking file under: \033[93m$DIR\033[0m"
 
 echo -e "\033[96mDot file\033[0m"
-for i in `ls -a $DIR | grep '^\.[^.]' | grep -v '^\.\(vim\|git\)$'`; do
+for i in $(ls -a "$DIR" | grep '^\.[^.]' | grep -v '^\.\(vim\|git\)$'); do
     if [ -e "$HOME/$i" ]
     then
         echo -e "\033[91mFound \033[93m$i\033[0m, skip linking, please merge manually."
     else
         echo -e "\033[92mNope \033[93m$i\033[0m, linking..."
-        ln -s $DIR/$i $HOME/$i
+        ln -s "$DIR/$i" "$HOME/$i"
     fi
 done
 
@@ -18,17 +18,20 @@ echo -e "\033[96mDot folder\033[0m"
 # vim 
 vimf=$DIR'/.vim/'
 vimhome=$HOME'/.vim/'
-for i in `ls $vimf`; do
+for f in "$vimf"/*; do
 
-    [ ! -d "$vimhome/$i" ]  && echo -e "\033[92mCreate folder: \033[93m$i\033[0m" && mkdir -p $vimhome/$i || echo -e "Folder exists: \033[93m$i\033[0m"
+    i=$(basename "$f")
 
-    for j in `ls $vimf/$i`; do
+    [ ! -d "$vimhome/$i" ]  && echo -e "\033[92mCreate folder: \033[93m$i\033[0m" && mkdir -p "$vimhome/$i" || echo -e "Folder exists: \033[93m$i\033[0m"
+
+    for f2 in "$vimf/$i"/*; do
+        j=$(basename "$f2")
         if [ -e "$vimhome/$i/$j" ]
         then
             echo -e "\033[91mFound \033[93m$i/$j\033[0m, skip linking, please merge manually."
         else
             echo -e "\033[92mNope \033[93m$i/$j\033[0m, linking..."
-            ln -s $vimf/$i/$j $vimhome/$i/$j
+            ln -s "$vimf/$i/$j" "$vimhome/$i/$j"
         fi
     done
 
