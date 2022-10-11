f6622bd1843c7d2ca27ab0056d468edb325c5376
Meng-Ying Yang <ymy1019@gmail.com>
2022-10-11 10:40:13 +0800

    setup_dev.sh: fix setup flow for re-run

--- setup_dev.sh
+++ setup_dev.sh
@@ -5,11 +5,7 @@ this="$(uname)"
 if [ "$this" == "Darwin" ]; then
     # OSX
     echo "=====> setup  homebrew..."
-    if [ ! -f /usr/local/bin/brew ]; then
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    else
-        echo "homebrew installed!"
-    fi
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     pkgi="brew install "
 elif [ "$this" == "Linux" ]; then
     # assume debian
@@ -25,6 +21,8 @@ echo "=====> installing tools"
 $pkgi git bash-completion make bison binutils gcc cmake curl tig tmux shellcheck cscope ctags
 
 echo "=====> setup TMUX plugin manager"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
-git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum
+if [ ! -d "$HOME/.tmux" ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
+    git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum
+fi
